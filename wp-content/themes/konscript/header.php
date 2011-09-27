666b049ccd05ef06942f975530f16f1b1a162477
laander <la@laander.com>
2011-09-27 12:56:25 +0200

    updated theme with boilerplate css/sass and header

--- wp-content/themes/konscript/header.php
+++ wp-content/themes/konscript/header.php
@@ -12,25 +12,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>
 <head>
-<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
-<title><?php hybrid_document_title(); ?></title>
-
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-<?php wp_head(); // wp_head ?>
-
+	
+	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+	<title><?php hybrid_document_title(); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/stylesheet/compiled/main.css" type="text/css" media="all" />
+	<script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascript/main.js"></script>
+	<?php wp_head(); // wp_head ?>
+	
 </head>
+<body class="bp <?php hybrid_body_class(); ?>">
 
-<body class="<?php hybrid_body_class(); ?>">
+	<div id="body-wrapper">
+		<div id="header-container">
+			<div id="header">
+				<?php do_atomic( 'header' ); // hybrid_header ?>
+				<?php get_template_part( 'menu', 'primary' ); ?>
+			</div><!-- #header -->
+		</div><!-- #header-container -->
 
-<div id="body-container">
-	<div id="header-container">
-		<div id="header">
-			<?php do_atomic( 'header' ); // hybrid_header ?>
-			<?php get_template_part( 'menu', 'primary' ); ?>
-		</div><!-- #header -->
-	</div><!-- #header-container -->
-
-	<div id="content-container">
+		<div id="content-container">
