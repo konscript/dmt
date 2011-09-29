e43ae1fa4c18ff6d7429b0759fe8045e3f5ed942
Thomas Didriksen <Thomas@Thomas-Didriksens-MacBook-Pro.local>
2011-09-29 23:58:51 +0200

    Continued word on main menu

--- wp-content/themes/konscript/menu-primary.php
+++ wp-content/themes/konscript/menu-primary.php
@@ -7,9 +7,10 @@
  */
 
 if ( has_nav_menu( 'primary' ) ) : ?>
-
 	<div id="primary-menu" class="menu-container">
-		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => '', 'fallback_cb' => '' ) );?>		
+		<span id="primary-menu-left-end"><img src="<?php bloginfo('template_url'); ?>/graphics/primary-menu_left.png"></span>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => '', 'fallback_cb' => '' ) );?>
+		<span id="primary-menu-right-end"><img src="<?php bloginfo('template_url'); ?>/graphics/primary-menu_right.png"></span>		
 	</div><!-- #primary-menu .menu-container -->
 
 <?php endif; ?>
