68a1a75912c32e91dd4650d6a9be3ef67ede0d98
laander <la@laander.com>
2011-11-16 15:06:45 +0100

    more small adjustments and improvements e.g. footer

--- wp-content/themes/konscript/footer.php
+++ wp-content/themes/konscript/footer.php
@@ -13,12 +13,18 @@
 	</div><!-- #body-wrapper -->
 	<div id="footer-container">
 		<div id="footer">
-		<!--
-			<span>Dansk Materiale Teknik A/S  |  Smedeholm 8, DK-2730 Herlev, Danmark</span>
-			<span>CVR: 25063481  |  dmt@dmtas.dk  |  +45 70201601</span>
-		-->
-		    <?php get_template_part( 'menu-footer' ) ?>
-		    <?php dynamic_sidebar( 'footer' ); ?>
+			<div id="footer-products">
+				<div class="footer-products-text">Se hvilke produkter vi udbyder indenfor:</div><br />
+				<a class="footer-button first" href="materialer/produkter">Materialer</a>
+				<a class="footer-button" href="vaerktoejer/vaerktoejer-produkter">Værktøjer</a>
+			</div>
+			<div id="footer-info">
+				<img src="<?php echo get_bloginfo ('template_url'); ?>/graphics/logo_footer.png" />				
+				<div>Dansk Materiale Teknik A/S  |  Smedeholm 8, DK-2730 Herlev, Danmark<br />
+				CVR: 25063481  |  dmt@dmtas.dk  |  +45 70201601</div>
+			</div>
+		    <?php //get_template_part( 'menu-footer' ) ?>
+		    <?php //dynamic_sidebar( 'footer' ); ?>
 			<?php //do_atomic( 'breadcrumb' ); // breadcrumb trail ?>
 		</div><!-- #footer -->
 	</div><!-- #footer-container -->
