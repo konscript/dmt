87facd81b5ce7ce00d0c8889cf9c915753bfd6a0
Thomas Didriksen <t.didriksen@gmail.com>
2011-11-20 15:00:37 +0100

    Localized loop-error.php, fixed small anomalies. Hopefully it works this time

--- wp-content/themes/konscript/loop-error.php
+++ wp-content/themes/konscript/loop-error.php
@@ -11,7 +11,7 @@
 	<div id="post-0" class="<?php hybrid_entry_class(); ?>">
 		<div class="entry-content">
 			<p class="no-data">
-				<?php _e( 'Der er desværre intet indhold på denne side.', hybrid_get_textdomain() ); // Apologies, but no results were found. ?>
+				<?php _e( 'Der er desværre intet indhold på denne side.', hybrid_get_textdomain() ); //Apologies, but no results were found. ?>
 			</p><!-- .no-data -->
 		</div><!-- .entry-content -->
 	</div><!-- .hentry .error -->
